0b97a1392e6669f36ec3b11f2e1c8d43c44542d7
zeyad ahmed <zeyadmokled@hotmail.com>
2023-03-15 17:42:10 +0200

    adding selection sort

---
+++ selection_sort.py
@@ -0,0 +1,14 @@
+def selection_sort(array):
+    index=0
+    for i in range(0,len(array)-1):
+        min = array[i]
+        for j in range(i+1, len(array) - 1):
+            if (array[j]<min):
+                min=array[j]
+                index=j
+        temp=array[i]
+        array[i]=min
+        array[index]=temp
+arrr=[2,99,635,-589,6453,-545,33,9,6,8,87,10,100,150,190,1000,9000]
+selection_sort(arrr)
+print(arrr)
