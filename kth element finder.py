19a411dfcf4b8b539d1fdbb1c4a45d5ad9b08f82
peter anton <97073590+Peter-Anton@users.noreply.github.com>
2023-03-18 05:09:02 +0200

    finish all+report

--- kth element finder.py
+++ kth element finder.py
@@ -2,6 +2,7 @@ import quick_sort_fr
 import random
 import quick_sort_fr
 
+
 def find_kth_element(arr, low, high, element):
     if high > low:
         pivot = quick_sort_fr.partition(arr, low, high)
